1a1c31068a4a9d34b3e7e38028ce455798f39d08
AQUIL3S <andreshermosodemendoza@gmail.com>
2020-12-04 14:35:33 +0100

    proyecto con buscador js(para checkar)

--- js/buscador.js
+++ js/buscador.js
@@ -4,17 +4,25 @@ let search = document.getElementById("search");
 btn.addEventListener("click", function(event){
     search.classList.remove("borderError");
 
-    let texto = search.value;
+    let texto = search.value.toLocaleLowerCase();
+    alert(search.value.toLocaleLowerCase());
     event.preventDefault()
-
+    console.log(document.getElementsByClassName("maiz"));
+    
     if(texto != ""){
-        if(texto.includes("Maiz")){
+        if(texto.includes("maiz") ){
             document.getElementById('error').innerHTML = ``
             //Ejecutar foco al enlace 1
+            //hace focus normal de navegador con su borde y un color de fondo añadido que igual sobra
+            document.getElementsByClassName("maiz")[0].focus();
+          
+            document.getElementsByClassName("maiz")[0].style.backgroundColor = "#00B6F1";
+            
 
-        }else if(texto.includes("manzanas")){
+        }else if(texto.includes("manzana")){
             document.getElementById('error').innerHTML = ``
             //Ejecutar foco al enlace 2
+            document.getElementsByClassName("manzana")[0].focus();
             
         }else{
             document.getElementById('error').innerHTML = `<div class="alert alert-danger" role="alert">Upss! no hemos encontrado nada relacionado con <strong>${texto}</strong> </div>`
